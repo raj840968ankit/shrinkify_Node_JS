e5dad991a62652d52529cdb97a3dce460d947a94
ankit <rajankit840968@gmail.com>
2025-06-11 19:22:09 +0530

    remove FRONTEND_URL from environment schema

--- config/env.js
+++ config/env.js
@@ -21,7 +21,6 @@ const envSchema = z.object({
     GOOGLE_CLIENT_SECRET : z.string().min(1),
     GITHUB_CLIENT_ID : z.string().min(1),
     GITHUB_CLIENT_SECRET : z.string().min(1),
-    FRONTEND_URL : z.string().url().trim().min(1),
 })
 
 export const env = envSchema.parse(process.env);
